fc27094ed725fd2efe9caa32b4c8e004ca3f3ad5
Teddy Bandama <bandama.teddy@gmail.com>
2022-02-14 16:27:02 +0100

    j'ai fini

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @message = "Great !"
+    elsif @question.include?("?")
+      @message = "Silly question, get dressed and go to work!."
+    else
+     @message = "I don't care, get dressed and go to work!."
+    end
+  end
+end
